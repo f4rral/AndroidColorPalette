a9b916bc7fd28bc0d5c349872a8135764adb5b6e
f4rral <f4rral@gmail.com>
2025-01-26 14:23:59 +0300

    Портретное и ландшафтное представления

--- app/src/main/java/com/example/colorpalette/MainActivity.kt
+++ app/src/main/java/com/example/colorpalette/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.colorpalette
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -22,6 +25,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -56,12 +60,53 @@ fun ColorDisplay(color: Color) {
                     shape = RoundedCornerShape(8.dp)
                 )
         )
+
         Spacer(modifier = Modifier.height(8.dp))
+
         Text(text = "RGB(" +
             "${(color.red * 255).toInt()}, " +
             "${(color.green * 255).toInt()}, " +
             "${(color.blue * 255).toInt()})"
         )
+    }
+}
+
+@Composable
+fun ColorPalette(
+    colorDisplay: @Composable () -> Unit,
+    colorSliders: @Composable () -> Unit
+) {
+    val configuration = LocalConfiguration.current
+    val orientation = configuration.orientation
+
+    if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            colorDisplay()
+
+            Spacer(modifier = Modifier
+                .height(32.dp)
+            )
+
+            colorSliders()
+        }
+    } else {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            colorDisplay()
+
+            Spacer(modifier = Modifier
+                .width(32.dp)
+            )
+
+            colorSliders()
+        }
     }
 }
 
@@ -74,46 +119,48 @@ fun ScreenMain() {
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.fillMaxWidth()
         ) {
-            Spacer(modifier = Modifier
-                .height(64.dp)
+            ColorPalette(
+                colorDisplay = {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        ColorDisplay(color = Color(
+                            red = viewModel.redColor,
+                            green = viewModel.greenColor,
+                            blue = viewModel.blueColor,
+                        ))
+                    }
+                },
+                colorSliders = {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.width(250.dp)
+                    ) {
+                        Button(
+                            onClick = { viewModel.generateRandomColor() }
+                        ) {
+                            Text(text = "Случайный")
+                        }
+
+                        Spacer(modifier = Modifier
+                            .height(32.dp)
+                        )
+
+                        Slider(
+                            value = viewModel.redColor.toFloat() / 255,
+                            onValueChange = { viewModel.redColor = (it * 255).toInt() }
+                        )
+                        Slider(
+                            value = viewModel.greenColor.toFloat() / 255,
+                            onValueChange = { viewModel.greenColor = (it * 255).toInt() }
+                        )
+                        Slider(
+                            value = viewModel.blueColor.toFloat() / 255,
+                            onValueChange = { viewModel.blueColor = (it * 255).toInt() }
+                        )
+                    }
+                }
             )
-
-            ColorDisplay(color = Color(
-                red = viewModel.redColor,
-                green = viewModel.greenColor,
-                blue = viewModel.blueColor,
-            ))
-
-            Spacer(modifier = Modifier
-                .height(32.dp)
-            )
-
-            Button(
-                onClick = { viewModel.generateRandomColor() }
-            ) {
-                Text(text = "Случайный")
-            }
-
-            Spacer(modifier = Modifier
-                .height(32.dp)
-            )
-
-            Column(
-                modifier = Modifier.width(250.dp)
-            ) {
-                Slider(
-                    value = viewModel.redColor.toFloat() / 255,
-                    onValueChange = { viewModel.redColor = (it * 255).toInt() }
-                )
-                Slider(
-                    value = viewModel.greenColor.toFloat() / 255,
-                    onValueChange = { viewModel.greenColor = (it * 255).toInt() }
-                )
-                Slider(
-                    value = viewModel.blueColor.toFloat() / 255,
-                    onValueChange = { viewModel.blueColor = (it * 255).toInt() }
-                )
-            }
         }
     }
 }
